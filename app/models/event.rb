84305b030953c851563849a3c6454be61ceedf37
ltciro <ltciro@gmail.com>
2016-04-18 13:52:00 -0500

    hashtag y crear evento

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+	include SimpleHashtag::Hashtaggable
 	belongs_to :user
 
 	has_many :inviteds
@@ -14,6 +15,8 @@ class Event < ActiveRecord::Base
 	has_many :userfeed, through: :feedbacks, :source => 'user'
 
 	mount_uploader :img, EventImgUploader
+	
+	hashtaggable_attribute :description
 	#validaciones
 	validates :title,:description,:name_place,:address,:img,:capacity,:user_id, presence: true
 	validates :des_partner, :presence => true, :if => :need_partner
